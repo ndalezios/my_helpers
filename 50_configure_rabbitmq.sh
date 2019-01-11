fe740ef86c08939acdc08e79b597ca645fd748d9
ndalezios <ndalezios@gmail.com>
2019-01-11 23:25:25 +0200

    Delete 50_configure_rabbitmq.sh

--- 50_configure_rabbitmq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rabbitmq-plugins list
-rabbitmq-plugins enable rabbitmq_management
-service rabbitmq-server restart
-rabbitmqctl change_password stackrabbit guest
-rabbitmqctl set_user_tags stackrabbit administrator
-
